207ebd540be2edb7c84ceddd9233c506e81bc2c1
Vincent Garbonick <vincent.garbonick@autobag.com>
2018-07-20 15:57:15 -0400

    add config to main

--- main.py
+++ main.py
@@ -2,27 +2,10 @@ import pyautogui
 
 import random 
 
-#variables concerning the swipe loop, passes is number of passes 
-passes = 10
-count = 0
-likeNum = 0
-hateNum = 0
-
-#variables concerning screen size, can be adjusted for different screens (like and dislike come from center of button)
-tindrLikeX = 1350
-tindrLikeY = 754
-
-tindrMedX = 1350
-tinerMedY = 734
-
-tindrDislikeX = 1300
-tindrDislikeY = 754
-
-#variables concerning duration of clicks and swipes
-clickClock = 0.5
-durClock = 0.8
-
-#add random swipe time 
+import sys 
+sys.path.insert(0, 'C:/Users/vincent.garbonick/Desktop/AutoclickV2/config')
+import config
+from varconfig import * 
 
 while count <= passes:
 
