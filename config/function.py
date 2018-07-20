2370083fb5c4cbda65bed75555c5d4791decd57e
Vincent Garbonick <vincent.garbonick@autobag.com>
2018-07-20 16:18:17 -0400

    add function.py, mod TODO, varconifg, main

---
+++ config/function.py
@@ -0,0 +1,7 @@
+#add functions for main.py 
+import random
+
+from varconfig import *
+
+#function to generate random position for like 
+def randX
--- config/varconfig.py
+++ config/varconfig.py
@@ -1,3 +1,6 @@
+#this is the module containing all variables used in the main body to make the program modular
+
+
 #variables concerning the swipe loop, passes is number of passes 
 passes = 10
 count = 0
@@ -7,12 +10,17 @@ hateNum = 0
 #variables concerning screen size, can be adjusted for different screens (like and dislike come from center of button)
 tindrLikeX = 1350
 tindrLikeY = 754
+#variable that measures the distance between 
+tindrLikeDif = 10
+
 
 tindrMedX = 1350
-tinerMedY = 734
+tindrMedY = 734
+tindrMedDif = 10
 
 tindrDislikeX = 1300
 tindrDislikeY = 754
+tindrDislikeDif = 10
 
 #variables concerning duration of clicks and swipes
 clickClock = 0.5
